9c28db56049cb5f307cebb9811c8a183e11dd1be
Rob Righter <rob.righter@pardot.com>
2012-10-23 11:50:22 -0400

    Upped memory_limit

--- attributes/default.rb
+++ attributes/default.rb
@@ -82,7 +82,7 @@ default['php']['url'] = 'http://us1.php.net/get'
 default['php']['version'] = '5.5.9'
 default['php']['checksum'] = '378de162efdaeeb725ed38d7fe956c9f0b9084ff'
 default['php']['prefix_dir'] = '/usr/local'
-default['php']['memory_limit'] = '128M'
+default['php']['memory_limit'] = '1024M'
 default['php']['timezone'] = 'US/Eastern'
 
 default['php']['configure_options'] = %W{--prefix=#{php['prefix_dir']}
