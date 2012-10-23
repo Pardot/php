292b86ab7eafa5fbca5417cc535581283326b06d
Rob Righter <rob.righter@pardot.com>
2012-10-23 10:07:10 -0400

    updated memory limit tuning and timezone for php.ini

--- attributes/default.rb
+++ attributes/default.rb
@@ -83,6 +83,7 @@ default['php']['version'] = '5.5.9'
 default['php']['checksum'] = '378de162efdaeeb725ed38d7fe956c9f0b9084ff'
 default['php']['prefix_dir'] = '/usr/local'
 default['php']['memory_limit'] = '128M'
+default['php']['timezone'] = 'US/Eastern'
 
 default['php']['configure_options'] = %W{--prefix=#{php['prefix_dir']}
                                          --with-libdir=#{lib_dir}
